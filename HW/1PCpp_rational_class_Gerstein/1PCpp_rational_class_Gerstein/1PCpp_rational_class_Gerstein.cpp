7e6fab755abf9166f2db854b147edcd5b92febcc
GersteinJo <71694809+GersteinJo@users.noreply.github.com>
2024-03-26 15:12:09 +0300

    Add files via upload

--- HW/1PCpp_rational_class_Gerstein/1PCpp_rational_class_Gerstein/1PCpp_rational_class_Gerstein.cpp
+++ HW/1PCpp_rational_class_Gerstein/1PCpp_rational_class_Gerstein/1PCpp_rational_class_Gerstein.cpp
@@ -62,59 +62,61 @@ public:
     }
 
 
-    friend std::ostream &operator << (std::ostream &out, Rational r)
+    friend std::ostream& operator << (std::ostream& out, Rational r)
     {
         return out << r._m << "/" << r._n;
     }
 
-    Rational &operator++(int)
+    Rational& operator++(int)
+    {
+        Rational res = *this;
+        _m += _n;
+        return res;
+    }
+
+    Rational& operator++()
     {
         _m += _n;
         return *this;
     }
 
-    Rational &operator++()
+    Rational operator--(int)
     {
-        _m += _n;
-        return *this;
-    }
-
-    Rational &operator--(int)
-    {
+        Rational res = *this;
         _m -= _n;
-        return *this;
+        return res;
     }
-    Rational &operator--()
+    Rational& operator--()
     {
         _m -= _n;
         return *this;
     }
 
-    Rational &operator-()
+    Rational& operator-()
     {
         _m = -_m;
         return *this;
     }
 
-    Rational &operator-(Rational &other)
+    Rational operator-(Rational& other)
     {
         Rational new_r(_m * other._n - other._m * _n, _n * other._n);
         return new_r;
     }
 
-    Rational &operator+(Rational &other)
+    Rational operator+(Rational& other)
     {
         Rational new_r(_m * other._n + other._m * _n, _n * other._n);
         return new_r;
     }
 
-    Rational &operator*(Rational &other)
+    Rational operator*(Rational& other)
     {
-        Rational new_r(_m * other._m , _n * other._n);
+        Rational new_r(_m * other._m, _n * other._n);
         return new_r;
     }
 
-    Rational &operator/(Rational& other)
+    Rational operator/(Rational& other)
     {
         Rational new_r(_m * other._n, _n * other._m);
         return new_r;
@@ -123,7 +125,7 @@ public:
     Rational& operator-=(Rational& other)
     {
         _m = _m * other._n - other._m * _n;
-        _n = _n* other._n;
+        _n = _n * other._n;
         this->Standardize();
         return *this;
     }
@@ -152,12 +154,15 @@ public:
         return *this;
     }
 
-    bool operator=(Rational& other)
+    bool operator=(Rational other)
     {
         this->_m = other._m;
         this->_n = other._n;
         return true;
     }
+
+    /* in visual studio also works declaration of operator Rational& operator + (Rational& other) and bool operator=(Rational& other), but other compilers
+    complain that a reference to a soon be freed memory is given*/
 
     bool operator==(Rational& other)
     {
@@ -190,7 +195,7 @@ Rational Bernully(int n)
     {
         Rational new_r(1, 1 + i);
         my_rs[i] = new_r;
- 
+
         for (int j = i; j > 0; j--)
         {
             Rational j_r(j);
@@ -233,9 +238,8 @@ int main()
         r1 += r2;
         r2 -= r1;
         std::cout << r1 << " " << r2 << "\n";
-        ++(r1++);
-        --(-r2--);
-        std::cout << r1 << " " << r2 << "\n";
+        Rational r4 = --r2++;
+        std::cout << r1 << " " << r2 << " " <<r4<<"\n";
         r3 = r1 / r2;
         std::cout << r3 << "\n";
         r3 = r1 - r2;
@@ -245,19 +249,19 @@ int main()
         std::cout << (r1 == r2) << "\n";
         r3 = r1.pow(4);
         std::cout << r3 << "\n";
-        std::cout << r3.To_Double()<<"\n *** \n";
+        std::cout << r3.To_Double() << "\n *** \n";
     }
 
 
     int bs_depth;
     std::cout << "Enter the expected depth of the sequence: ";
     std::cin >> bs_depth;
-    Rational* bs = Bernully_sequence(bs_depth+1);
+    Rational* bs = Bernully_sequence(bs_depth + 1);
     std::cout << "Bernully sequence of " << bs_depth << " elements:\n";
-    for (int n = 0; n < bs_depth+1; n++) std::cout << bs[n]<<" ";
-    std::cout << "\n B_2k sequence of " << bs_depth/2 << " elements:\n";
-    Rational* bs_2k = new Rational[bs_depth / 2+1];
-    for (int n = 0; n < bs_depth / 2+1; n++)
+    for (int n = 0; n < bs_depth + 1; n++) std::cout << bs[n] << " ";
+    std::cout << "\n B_2k sequence of " << bs_depth / 2 << " elements:\n";
+    Rational* bs_2k = new Rational[bs_depth / 2 + 1];
+    for (int n = 0; n < bs_depth / 2 + 1; n++)
     {
         bs_2k[n] = bs[2 * n];
         std::cout << bs_2k[n] << " ";
