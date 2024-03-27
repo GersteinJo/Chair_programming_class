21c1a4430e9621e7a4269c5bd45171a2837d259e
GersteinJo <71694809+GersteinJo@users.noreply.github.com>
2024-03-27 04:05:42 +0300

    Add files via upload

---
+++ HW/2PCpp_Polynomial_Gershtein/2PCpp_Polynomial_Gershtein/2PCpp_Polynomial_Gershtein.cpp
@@ -0,0 +1,258 @@
+﻿// 2PCpp_Polynomial_Gershtein.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+
+class Polynomial
+{
+public:
+    Polynomial()
+    {
+        coefficients = { 0 };
+    }
+
+    Polynomial(std::vector<double>& coefs)
+    {
+        coefficients.clear();
+        std::copy(coefs.begin(), coefs.end(), std::back_inserter(coefficients));
+    }
+
+    Polynomial& operator-();
+
+    Polynomial operator-(const Polynomial& other);
+    Polynomial operator+(const Polynomial& other);
+    Polynomial operator*(const Polynomial& other);
+
+    Polynomial& operator+=(const Polynomial& other);
+    Polynomial& operator-=(const Polynomial& other);
+    Polynomial& operator*=(const Polynomial& other);
+
+    Polynomial derivative() const;
+    double derivative(double x) const;
+
+    double operator()(double x);
+
+    friend std::ostream& operator<<(std::ostream& out, const Polynomial& other);
+
+private:
+    std::vector<double> coefficients;
+};
+
+Polynomial& Polynomial::operator-()
+{
+    std::for_each(coefficients.begin(), coefficients.end(), [](double x) { return -x; });
+    return *this;
+}
+
+Polynomial Polynomial::operator-(const Polynomial& other)
+{
+    std::vector<double> new_coefs(std::max(other.coefficients.size(), coefficients.size()));
+    if (other.coefficients.size() > coefficients.size())
+    {
+        int tmp = other.coefficients.size() - coefficients.size();
+        for (int i = 0; i < tmp; i++) coefficients.push_back(0.0);
+    }
+    for (int i = 0; i < other.coefficients.size(); i++)
+    {
+        new_coefs[i] = coefficients[i] - other.coefficients[i];
+    }
+    Polynomial res(new_coefs);
+    return res;
+}
+
+Polynomial Polynomial::operator+(const Polynomial& other)
+{
+    std::vector<double> new_coefs(std::max(other.coefficients.size(), coefficients.size()));
+    if (other.coefficients.size() > coefficients.size())
+    {
+        int tmp = other.coefficients.size() - coefficients.size();
+        for (int i = 0; i < tmp; i++) coefficients.push_back(0.0);
+    }
+    for (int i = 0; i < other.coefficients.size(); i++)
+    {
+        new_coefs[i] = coefficients[i] + other.coefficients[i];
+    }
+    Polynomial res(new_coefs);
+    return res;
+}
+
+Polynomial Polynomial::operator*(const Polynomial& other)
+{
+    std::vector<double> new_coefs;
+    for (int i = 0; i < coefficients.size() + other.coefficients.size()-1; i++) new_coefs.push_back(0.0);
+    for (int i = 0; i < coefficients.size(); i++)
+    {
+        for (int j = 0; j < other.coefficients.size(); j++)
+        {
+            new_coefs[i + j] += coefficients[i] * other.coefficients[j];
+        }
+    }
+    Polynomial res(new_coefs);
+    return res;
+}
+
+Polynomial& Polynomial::operator-=(const Polynomial& other)
+{
+    if (other.coefficients.size() > coefficients.size())
+    {
+        int tmp = other.coefficients.size() - coefficients.size();
+        for (int i = 0; i < tmp; i++) coefficients.push_back(0.0);
+    }
+    for (int i = 0; i < other.coefficients.size(); i++)
+    {
+        coefficients[i] -= other.coefficients[i];
+    }
+    return *this;
+}
+
+Polynomial& Polynomial::operator+=(const Polynomial& other)
+{
+    if (other.coefficients.size() > coefficients.size())
+    {
+        int tmp = other.coefficients.size() - coefficients.size();
+        for (int i = 0; i < tmp; i++) coefficients.push_back(0.0);
+    }
+    for (int i = 0; i < other.coefficients.size(); i++)
+    {
+        coefficients[i] += other.coefficients[i];
+    }
+    return *this;
+}
+
+Polynomial& Polynomial::operator*=(const Polynomial& other)
+{
+    std::vector<double> new_coefs;
+    for (int i = 0; i < coefficients.size() + other.coefficients.size(); i++) new_coefs.push_back(0.0);
+
+    for (int i = 0; i < coefficients.size(); i++)
+    {
+        for (int j = 0; j < other.coefficients.size(); i++)
+        {
+            new_coefs[i + j] += coefficients[i] * other.coefficients[j];
+        }
+    }
+    coefficients.clear();
+    std::copy(new_coefs.begin(), new_coefs.end(), std::back_inserter(coefficients));
+    return *this;
+}
+
+Polynomial Polynomial::derivative() const
+{
+    std::vector<double>vec(coefficients.size() - 1);
+    for (int i = 1; i < coefficients.size(); i++)
+    {
+        vec[i - 1] = (double)i * coefficients[i];
+    }
+    Polynomial res(vec);
+    return res;
+}
+
+double Polynomial::derivative(double x) const
+{
+    std::vector<double>vec(coefficients.size() - 1);
+    for (int i = 1; i < coefficients.size(); i++)
+    {
+        vec[i - 1] = (double)i * coefficients[i];
+    }
+    Polynomial res(vec);
+    return res(x);
+}
+
+double Polynomial::operator()(double x)
+{
+    double sum = coefficients[coefficients.size() - 1];
+    for (int i = coefficients.size()-2; i>-1; i--) sum = sum * x + coefficients[i];
+    return sum;
+}
+
+std::ostream& operator<<(std::ostream& out, const Polynomial& other)
+{
+    bool not_first = false;
+    if (other.coefficients[0] != 0)
+    {
+        out << other.coefficients[0];
+        not_first = true;
+    }
+    if (other.coefficients.size() > 1)
+    {
+        if (other.coefficients[1] > 0 && not_first)
+        {
+            if (other.coefficients[1] == 1.) out << "+" << "x";
+            else out << "+" << other.coefficients[1]<<"x";
+        }
+        else if (other.coefficients[1] < 0 || !not_first)
+        {
+            if (other.coefficients[1] == -1.) out << "-" << "x";
+            else out << other.coefficients[1] <<"x";
+            not_first = true;
+        }
+    }
+    for (int i = 2; i < other.coefficients.size(); i++)
+    {
+        if (other.coefficients[i] > 0 && not_first)
+        {
+            if (other.coefficients[i] == 1.) out << "+" << "x^" << i;
+            else out << "+" << other.coefficients[i] << "x^" << i;
+        }
+        else if (other.coefficients[i] < 0 || !not_first)
+        {
+            if(other.coefficients[i]==-1)out <<  "-x^" << i;
+            else out << other.coefficients[i] << "x^" << i;
+            not_first = true;
+        }
+    }
+    if (!not_first) out << 0;
+    return out;
+}
+
+double ln(double x)
+{
+    return std::log(x + 1.);
+}
+double d_ln(double x)
+{
+    return 1. / (x + 1.);
+}
+
+double sinx(double x)
+{
+    return std::sin(x) / x;
+}
+
+double d_sinx(double x)
+{
+    return std::cos(x) / x - std::sin(x)/x/x;
+}
+
+int main()
+{
+    std::vector<double> vln = { 0., 1., -1. / 2., 1. / 3., -1. / 4., 1. / 5. };
+    std::vector<double> vsn = { 1., 0., -1. / 6., 0., -1. / 120., 0., 1./5040.};
+    Polynomial ln_pol(vln);
+    Polynomial sn_pol(vsn);
+    std::cout << ln_pol << " " << sn_pol << "\n";
+    std::cout << ln_pol + sn_pol << "\n";
+    std::cout << ln_pol - sn_pol << "\n";
+    std::cout << ln_pol * sn_pol << "\n";
+    double x = 0.05;
+    std::cout << ln_pol.derivative() << "=" << ln_pol.derivative(x) << " || " << d_ln(x) << "\n";
+    std::cout << sn_pol.derivative() << "=" << sn_pol.derivative(x) << " || " << d_sinx(x) << "\n";
+    std::cout << ln_pol(x) << " " << ln(x) << " || " << sn_pol(x) << " " << sinx(x) << "\n";
+    std::cout << (ln_pol * sn_pol)(x) << " " << ln(x) * sinx(x) << "\n";
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ HW/3Cpp_phone_book_Gerstein/3Cpp_phone_book_Gerstein/3Cpp_phone_book_Gerstein.cpp
@@ -0,0 +1,89 @@
+﻿// 3Cpp_phone_book_Gerstein.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <map>
+#include <string>
+
+class PhoneBook
+{
+public:
+    PhoneBook() = default;
+    
+    std::string& operator[](std::string name)
+    {
+        return dict_name_num[name];
+    }
+    void AddUser(std::string name, std::string number)
+    {
+        if (dict_name_num.count(name) > 0)std::cout << "Name "<<name<<" is already in the database! Overriding\n";
+        dict_name_num[name] = number;
+    }
+    std::string FindNumber(std::string name) const
+    {
+        return dict_name_num.find(name)->second;
+    }
+    void DeleteNumber(std::string name)
+    {
+        dict_name_num.erase(name);
+    }
+    void Print();
+    void Print(std::string num);
+
+private:
+    std::map<std::string, std::string> dict_name_num;
+};
+
+void PhoneBook::Print()
+{
+    std::cout << "***Phone book***\n";
+    std::map<std::string, std::string>::iterator it;
+    for (it = dict_name_num.begin(); it != dict_name_num.end(); it++)
+        std::cout << it->first << "\t\t" << it->second << "\n";
+    std::cout << "***\n";
+}
+
+void PhoneBook::Print(std::string num)
+{
+    std::cout << "***\n";
+    std::cout << num << ":\n";
+    std::map<std::string, std::string>::iterator it;
+    bool found_non = true;
+    for (it = dict_name_num.begin(); it != dict_name_num.end(); it++)
+    {
+        if (it->second == num)
+        {
+            std::cout << it->first << "\n";
+            found_non = false;
+        }
+    }
+    if (found_non) std::cout << "No such number in the book\n";
+    std::cout << "***\n";
+}
+
+int main()
+{
+    PhoneBook my_phone_book;
+    my_phone_book["Jephry"] = "123456789";
+    my_phone_book["Holy"] = "123456545";
+    my_phone_book["Janine"] = "098765432";
+    my_phone_book["Barry"] = "123456789";
+    my_phone_book.Print();
+    my_phone_book.AddUser("Janine", "123");
+    std::cout<<"Let's find out Barry's number: " << my_phone_book.FindNumber("Barry") << "\n";
+    my_phone_book.DeleteNumber("Holy");
+    my_phone_book.Print();
+    my_phone_book.Print("123456789");
+    my_phone_book.Print("1");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
